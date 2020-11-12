7fd0af453f23877d8b32d4489d3e9f2b1219bd9a
jfollert <jfollert@alumnos.inf.utfsm.cl>
2020-11-12 14:09:15 -0300

    Nueva funcionalidad para comparar multiples imágenes en una ejecución

--- README.md
+++ README.md
@@ -1,21 +1,35 @@
 # AWS Rekognition Text in Image Comparator
-Aplicativo desarrollado en *Python 3.7* que utiliza AWS para verificar que una imagen contenga el mismo texto que una imagen de control.
+Aplicativo desarrollado en *Python 3.7* que utiliza servicios de AWS para verificar que una imagen contenga el mismo texto que una imagen de control.
 Para el almacenamiento de las imagenes se utilizará **S3** y para la extracción del texto se utilizará **Rekognition**.
 
 ## Consideraciones
+- El programa puede procesar un máximo 50 palabras por imagen.
 - El programa no diferencia entre mayúsculas y minúsculas.
-- Se ignoraran los siguientes caracteres para efecto de la comparación -> `!.`
+- Se ignoraran los siguientes caracteres para efecto de la comparación: `¡!:;,.@/$%-_+*#'`
+- El texto debe estar dentro de una orientación de +/- 90° dentro de la imagen
+- Se define la **confianza mínima** para la detección de texto como un 97%
+- Las extensiones para archivos de imagen permitidos son: `.png, .jpg, .jpeg`
 
 ## Instalación
-Requiere tener instalado **Python 3.7** o una versión mayor, además de tener instalado [**AWS CLI version 2**](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html).
+Se Requiere instalar **Python 3.7** o una versión mayor, además de tener instalado [**AWS CLI version 2**](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html).
 
-Además, requiere instalar la librería `boto3`, para hacerlo con pip se puede utilizar el siguiente comando:
+Se requiere instalar el módulo de python `boto3`. Para hacerlo con pip se puede utilizar el siguiente comando:
 ```bash
 pip install boto3
 ```
+Finalmente, es necesario tener actualizado el archivo de `./aws/credentials` con las respectivas credenciales de AWS.
 
 ## Instrucciones de Uso
-Para ejecutar el programa debe utilizar la siguiente estructura:
+Para ejecutar el programa se debe utilizar el siguiente comando:
 ```bash
-python comparator.py -b <bucket> -c <controlfile> -t <testfile>
+python comparator.py -b <bucket> -c <controlfile> [-t <testfile> | -T <testdir>]
 ```
+#### Opciones:
+- `-b <bucket>` :  El nombre del S3 Bucket
+- `-c <controlfile>` :  Nombre de la imagen de control. Almacenada en el bucket
+- `-t <testfile>` :  Nombre de la imagen a probar. Almacenada en el bucket
+- `-T <testdir>` :  Nombre del directorio con las imagenes a probar. Dentro del bucket
+
+**NOTA-1:** Para su ejecución, el programa requiere obligatoriamente recibir argumentos para `<bucket>` y `<controlfile>`
+
+**NOTA-2:** Solamente se recibe un nombre de imagen a probar (`<testfile>`) o un nombre de directorio (`<testdir>`), no ambos. En caso de no recibir ninguno, el programa utilizará el directorio raíz del bucket como `<testdir>`
--- comparator.py
+++ comparator.py
@@ -5,48 +5,99 @@ import sys, getopt
 import logging
 
 MIN_CONFIDENCE = 0.97
-IGNORED_CHARS = ['!', '.']
+IGNORED_CHARS = ['¡', '!', ':', ';', ',', '.', '@', '/', '$', '%', '-', '_', '+', '*', '#', "'"]
+AUTHORIZED_EXTS = ["jpg", "jpeg", "png"]
 
-# Configuración del logger
-logger = logging.getLogger('root')
-logger.setLevel(logging.DEBUG)
+def compareText(test_words, control_words):
+    print(test_words)
+    for word in test_words:
+        if word not in control_words:
+            print(word)
+            return "false"
+    return "true"
 
-file_handler = logging.FileHandler('comparator.log', 'a', 'utf-8')
-file_handler.setLevel(logging.DEBUG)
+def isImageExtension(filename):
+    extension = filename.split('.')[-1]
+    if extension in AUTHORIZED_EXTS:
+        return True
+    return False
 
-formatter = logging.Formatter('%(asctime)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+def listBucketImages(s3, bucket, dir, control_img):
+    all_objects = s3.list_objects_v2(Bucket = bucket, Prefix = dir)
+    test_img = []
+    for obj in all_objects['Contents']:
+        key = obj['Key']
+        path = key.split("/")
+        #Verificar que en caso de que no se haya especificado un directorio solo se considere la raíz del backet
+        if len(path) != 1 and dir == '':
+            continue
+        filename = path[-1]
+        # Verificar que el nombre (key) tenga una extensión válida
+        if isImageExtension(filename):
+            # Verificar no considerar la imagen de control para test.
+            if key != control_img:
+                test_img.append(key)
+    print(test_img)
+    return test_img
 
 def getArgs():
     bucket = ''
     control_img = ''
     test_img = ''
+    test_dir = ''
 
     # Recibir como parámetros los nombres del bucket, imagen de control e imagen de prueba
     try:
-        opts, _ = getopt.getopt(sys.argv[1:],"hb:c:t:",["bucket=", "controlfile=","testfile="])
+        opts, _ = getopt.getopt(sys.argv[1:],"hb:c:t:T:")
     except getopt.GetoptError:
-        print('comparator.py -b <bucket> -c <controlfile> -t <testfile>')
+        print("USAGE")
+        print('comparator.py -b <bucket> -c <controlfile> [-t <testfile> | -T <testdir>]') 
+        print("for help use -h option")
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('comparator.py -b <bucket> -c <controlfile> -t <testfile>')
+            print("=> USAGE")
+            print('comparator.py -b <bucket> -c <controlfile> [-t <testfile> | -T <testdir>]')
+            print("=> HELP")
+            print("-b <bucket> :  S3 Bucket Name")
+            print("-c <controlfile> :  Name of the control image in the Bucket")
+            print("-t <testfile> :  Name of the test image in the Bucket")
+            print("-T <testdir> :  Name of the dir with the test images in the Bucket")
+            print("NOTE-1: Arguments are required for <bucket> and <controlfile>")
+            print("NOTE-2: Only one filename or one directory can be received for testing, not both")
             sys.exit()
         elif opt in ("-b", "--bucket"):
             bucket = arg
-        elif opt in ("-c", "--controlfile"):
+        elif opt in ("-c"):
             control_img = arg
-        elif opt in ("-t", "--testfile"):
+        elif opt in ("-t"):
             test_img = arg
+        elif opt in ("-T"):
+            test_dir = arg
     
-    # Verificar que se recibieron todos los argumentos
-    if bucket == '' or control_img == '' or test_img == '':
-        print ('comparator.py -b <bucket> -c <controlfile> -t <testfile>')
-        sys.exit()
-    
-    return bucket, control_img, test_img
+    # Verificar que se recibieron todos los argumentos obligatorios
+    if bucket == '' or control_img == '':
+        print("Arguments are required for <bucket> and <controlfile>")
+        print("=> USAGE")
+        print ('comparator.py -b <bucket> -c <controlfile> [-t <testfile> | -T <testdir>]')
+        print("for help use -h option")
+        sys.exit(2)
 
+    # Verificar la extension de la imagen de control
+    if not isImageExtension(control_img):
+        print("The control image file must have a .jpg .jpeg or .png extension")
+        print("for help use -h option")
+        sys.exit(2)
+    
+    # Verificar que no se recibieron simultaneamente nombre de imagen y directorio para pruebas.
+    if test_img != '' and test_dir != '':
+        print("Only one filename or one directory can be received for testing, not both")
+        print("=> USAGE")
+        print ('comparator.py -b <bucket> -c <controlfile> [-t <testfile> | -T <testdir>]')
+        print("for help use -h option")
+        sys.exit(2)
+    
+    return bucket, control_img, test_img, test_dir
 
 def rekognitionTextDetection(client, bucket, image):
     response = client.detect_text(Image = {'S3Object': {'Bucket': bucket,'Name': image}}, 
@@ -58,30 +109,50 @@ def rekognitionTextDetection(client, bucket, image):
             word = text['DetectedText'].lower() # Transformar toda la palabra a minúscula
             word = ''.join(i for i in word if not i in IGNORED_CHARS) # Filtrar los caracteres a ignorar
             detected.append(word)
-    return detected
-
+    return detected        
 
 if __name__ == "__main__":
+    # Configuración del logger
+    logger = logging.getLogger('root')
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler('comparator.log', 'a', 'utf-8')
+    file_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
     # Recibir y verificar los argumentos del programa 
-    bucket, control_img, test_img = getArgs()
+    bucket, control_img, test_img, test_dir = getArgs()
+
+    # Instanciar cliente de S3
+    s3 = boto3.client("s3")
+
+    # En caso de no recibir un nombre de imagen, obtenemos la lista de imagenes en el directorio del bucket
+    if test_img == '':
+        test_list = listBucketImages(s3, bucket, test_dir, control_img)
+    # En caso contrario encapsulamos el nombre de la imagen en una lista para iterarlo
+    else:
+        test_list = list(test_img)
 
     # Instanciar cliente de AWS Rekognition
     client = boto3.client('rekognition')
 
-    # Reconocer texto en las imagenes
+    # Reconocer texto en la imagen de control
     control_words = rekognitionTextDetection(client, bucket, control_img)
-    test_words = rekognitionTextDetection(client, bucket, test_img)
-
-    # Comparar el texto en las imagenes
-    resultado = "true"
-    for word in test_words:
-        if word not in control_words:
-            resultado = "false"
-    print(resultado)
     
-    #Genera el mensaje de log y actualiza el archivo
-    correct_log = '[%s] %s, %s -> %s'
-    logger.info(correct_log, bucket, control_img, test_img, resultado)
+    #Iterar
+    for image in test_list:
+        #Reconocer el texto de la imagen a probar
+        test_words = rekognitionTextDetection(client, bucket, image)
+
+        # Comparar el texto en las imagenes
+        resultado = compareText(test_words, control_words)
+        print(resultado)
+    
+        #Genera el mensaje de log y actualiza el archivo
+        correct_log = '[%s] %s, %s -> %s'
+        logger.info(correct_log, bucket, control_img, image, resultado)
 
     sys.exit()
